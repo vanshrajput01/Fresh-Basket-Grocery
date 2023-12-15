2922a8dd6c738457526065415d9f84560cf710a9
vanshrajput01 <rajputvansh009@gmail.com>
2023-12-15 21:16:47 +0530

    "search section done"

---
+++ Code/index.js
@@ -0,0 +1,53 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
+
+  window.onscroll = function() {myFunction()};
+
+  var navbar = document.getElementById("nav-bar");
+  var sticky = navbar.offsetTop;
+  
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+      navbar.style.backgroundColor = "white"
+      navbar.style.marginTop = "-5px"
+      navbar.style.border = "1px solid rgb(211, 211, 211)"
+
+
+      
+    } else {
+      navbar.classList.remove("sticky");
+      navbar.style.marginTop = "20px";
+      navbar.style.border = "1px solid white"
+
+  
+    }
+  }
+
+let swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
