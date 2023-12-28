98920b1e69c6d3ed1db62b75edd84baec7bb7315
vanshrajput01 <rajputvansh009@gmail.com>
2023-12-28 15:03:08 +0530

    some more update in website

---
+++ Code/shop.js
@@ -0,0 +1,52 @@
+const mobile_nav = document.querySelector('.mobile-navbar-btn')
+const navHeader = document.querySelector('.header');
+
+const toggleNavBar = () =>{
+
+  navHeader.classList.toggle("active")
+  
+};
+
+mobile_nav.addEventListener("click",()=> toggleNavBar());
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
+
+
+  if(window.outerWidth > 1024){
+    window.onscroll = function() {myFunctionNav()};
+  
+    var navbar = document.getElementById("nav-bar");
+    var sticky = navbar.offsetTop;
+    
+    function myFunctionNav() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+        navbar.style.backgroundColor = "white"
+        navbar.style.marginTop = "-5px"
+        navbar.style.border = "1px solid rgb(211, 211, 211)"
+  
+  
+        
+      } else {
+        navbar.classList.remove("sticky");
+        navbar.style.marginTop = "20px";
+        navbar.style.border = "1px solid white"
+  
+    
+      }
+    }
+  
+  }
